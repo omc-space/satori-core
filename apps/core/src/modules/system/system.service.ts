4c7ff10ec2857b37f75672fc5ee75fbf6849b754
dva-ren <iiuzrw@gmail.com>
2024-03-03 19:31:57 +0800

    chore: add default value

--- apps/core/src/modules/system/system.service.ts
+++ apps/core/src/modules/system/system.service.ts
@@ -22,7 +22,62 @@ export class SystemService {
     const result = await this.systemModel.findOne().lean()
     if (!result)
       await this.systemModel.create({
-        keys: {},
+        keys: {
+          header: [
+            {
+              name: '首页',
+              path: '/',
+              iconClass: 'i-tabler:alpha',
+              children: [
+                {
+                  name: '自述',
+                  path: '/about',
+                },
+                {
+                  name: '留言',
+                  path: '/message',
+                },
+                {
+                  name: '时间线',
+                  path: '/timeline',
+                },
+              ],
+            },
+            {
+              name: '文稿',
+              path: '/post',
+              iconClass: 'i-tabler:file-description',
+              children: [],
+            },
+            {
+              name: '手记',
+              path: '/note/latest',
+              iconClass: 'i-tabler:notebook',
+            },
+            {
+              name: '速览',
+              path: '/timeline',
+              iconClass: 'i-tabler:list-details',
+            },
+            {
+              name: '友链',
+              path: '/friends',
+              iconClass: 'i-tabler:link',
+            },
+            {
+              name: '更多',
+              iconClass: 'i-tabler:alphabet-cyrillic',
+              path: '/more',
+              children: [
+                {
+                  name: '一言',
+                  path: '/more/say',
+                  iconClass: 'i-tabler:music',
+                },
+              ],
+            },
+          ],
+        },
       })
   }
 
